4e2497e551cdb11e6cd4cc597b4d0966807db8ac
SeonYoung Joung <88700744+seonyo@users.noreply.github.com>
2023-08-11 23:01:43 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18004 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main (String args[]){
+        Scanner scan  = new Scanner (System.in);
+        double a, b;
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+        System.out.println(a/b);
+    }
+}
