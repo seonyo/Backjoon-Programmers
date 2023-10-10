f0b0784b58b99b0eaa12835e9acf4577eb4bcd24
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2023-10-11 02:37:46 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+    var answer = 0;
+    var cnt=0;
+    for (var i=left; i<=right; i++){
+        for(var j=1; j<=i; j++){
+            if(i%j==0) cnt++;
+        }
+        answer = cnt%2===0 ? answer+=i : answer-=i;
+        cnt=0;
+    }
+    return answer;
+}
