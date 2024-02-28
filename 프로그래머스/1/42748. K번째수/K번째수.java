b7cd16a2e1c1cf8e993c3f913242362943fb2a74
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-02-29 01:15:51 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int [commands.length];
+        for(int i=0; i<commands.length; i++){
+            int[] tempArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(tempArray);
+            answer[i] = tempArray[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
