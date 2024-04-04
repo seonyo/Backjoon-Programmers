657b1174ad49649ada7d1f909609da6380ed12d5
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-04-05 01:16:59 +0900

    [level 1] Title: 과일 장수, Time: 0.34 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,19 +1,12 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Collections.reverseOrder());
         
-        if(tmp.length % m != 0) {
-            tmp = Arrays.copyOfRange(tmp, 0, tmp.length - (tmp.length % m));
-        }
-                
-        for(int i=0; i<tmp.length; i+=m){
-            Integer[] sliceTmp = Arrays.copyOfRange(tmp, i, i+m);
-            answer+= sliceTmp[sliceTmp.length-1] * m;
+        Arrays.sort(score);
+        
+        for(int i = score.length; i>=m; i-=m){
+            answer += score[i - m] * m;
         }
         return answer;
     }
