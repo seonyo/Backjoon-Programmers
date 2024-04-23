7aa20c1f08073b6310865aea6a1807cc67d4db83
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-04-24 08:35:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 7.95 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] arr) {
+        int lcm = 0;
+        for(int i=0; i<arr.length; i++){
+            if(i == 0){
+                lcm = getLcm(arr[i], arr[i+1]);
+                i = 1;
+            }
+            else{
+                System.out.println(lcm + " " + arr[i]);
+                lcm = getLcm(lcm, arr[i]);
+            } 
+        }
+        return lcm;
+    }
+    
+    int getLcm(int num1, int num2){
+        int max = num1 >= num2 ? num1 : num2;
+        int min = num1 < num2 ? num1: num2;
+        int gcd = 1;
+        for(int i=1; i<=min; i++){
+            if(max % i == 0 && min % i == 0)
+                gcd = i;
+        }
+        
+        return num1 * num2 / gcd;
+    }
+}
