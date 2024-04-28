000c4c039e71c3d2b050bedf4a8b83372c670f4c
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-04-28 23:16:31 +0900

    [level 2] Title: 올바른 괄호, Time: 4.97 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 5.61 ms
+메모리: 53.6 MB, 시간: 4.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 22:59:4
+2024년 04월 28일 23:16:30
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,15 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        //String temp[] = s.split("");
-        int cnt=0;
+        int cnt = 0;
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='('){
+            if(s.charAt(i) == '(')
                 cnt++;
-            } else{
-                if(cnt <= 0){
-                    return false;
-                } else{
-                    cnt--;
-                }
+            else{
+                if(cnt <=0) return false;
+                cnt--;
             }
         }
-        
-        if(cnt != 0) answer = false;
-
-        return answer;
+        return cnt == 0 ? true : false;
     }
 }
