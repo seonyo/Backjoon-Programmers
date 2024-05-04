bf38372a0b46ef26f1af0cc8a40002c65719e6f7
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-05-05 03:14:00 +0900

    [Bronze II] Title: 수 정렬하기, Time: 348 ms, Memory: 22564 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -3,21 +3,14 @@ public class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N];
+        HashSet <Integer> set = new HashSet<>();
         for(int i=0; i<N; i++){
-            arr[i] = sc.nextInt();
+            set.add(sc.nextInt());
         }
-        for(int i=0; i<N-1; i++){
-            for(int j=0; j<N-1; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
+        ArrayList <Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
         }
     }
 }
